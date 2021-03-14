4024e7b0acc50d0e97e2c4e1d67d4fbba868bc15
itsneyoa <itstomsmart@icloud.com>
2021-03-14 23:45:04 +0000

    Fixed reloading with aliases

--- commands/utility/reload.js
+++ commands/utility/reload.js
@@ -3,33 +3,39 @@ const fs = require('fs');
 const config = require('../../config.json');
 
 module.exports = {
-	name: 'reload',
+    name: 'reload',
     aliases: ['restart', 'r'],
-	description: 'Reloads a command',
-	execute(message, args) {
+    description: 'Reloads a command',
+    execute(message, args) {
         if (!isOwner(message.author.id)) return message.channel.send(
             new Discord.MessageEmbed()
-            .setDescription(`Sorry, you don't have permission to do this`)
-            .setColor('DC143C')
+                .setDescription(`Sorry, you don't have permission to do this`)
+                .setColor('DC143C')
         )
 
-		if (!args.length) return message.channel.send(
+        if (!args.length) return message.channel.send(
             new Discord.MessageEmbed()
-            .setDescription(`You need to specify a command to reload`)
-            .setColor('DC143C')
+                .setDescription(`You need to specify a command to reload`)
+                .setColor('DC143C')
         );
         const commandName = args[0].toLowerCase();
         const command = message.client.commands.get(commandName)
-	        || message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+            || message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
         if (!command) return message.channel.send(
             new Discord.MessageEmbed()
-            .setDescription(`There is no command with name or alias \`${commandName}\``)
-            .setColor('DC143C')
+                .setDescription(`There is no command with name or alias \`${commandName}\``)
+                .setColor('DC143C')
         );
 
         const commandFolders = fs.readdirSync('./commands');
-        const folderName = commandFolders.find(folder => fs.readdirSync(`./commands/${folder}`).includes(`${commandName}.js`));
+        var folderName;
+        commandFolders.forEach(folder => {
+            const commandSubFolders = fs.readdirSync(`./commands/${folder}`);
+            commandSubFolders.forEach(cmd => {
+                if(cmd == `${command.name}.js`) folderName = folder;
+            });
+        });
 
         delete require.cache[require.resolve(`../${folderName}/${command.name}.js`)];
 
@@ -38,20 +44,20 @@ module.exports = {
             message.client.commands.set(newCommand.name, newCommand);
             return message.channel.send(
                 new Discord.MessageEmbed()
-                .setDescription(`Command \`${command.name}\` successfully reloaded!`)
-                .setColor('7CFC00')
+                    .setDescription(`Command \`${command.name}\` successfully reloaded!`)
+                    .setColor('7CFC00')
             )
         } catch (error) {
             console.error(error);
             return message.channel.send(
                 new Discord.MessageEmbed()
-                .setDescription(`Command \`${command.name}\` could not be reloaded.`)
-                .setColor('DC143C')
-            )   
+                    .setDescription(`Command \`${command.name}\` could not be reloaded.`)
+                    .setColor('DC143C')
+            )
         }
-	},
+    },
 };
 
-function isOwner(member){
+function isOwner(member) {
     return member == config.discord.ownerId;
 }
