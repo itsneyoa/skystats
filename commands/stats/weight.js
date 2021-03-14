3da2a7a3c2f288900a99b58e2a32c774bf482b6a
neyoa <itstomsmart@gmail.com>
2021-03-14 20:38:18 +0000

    Formatted weight command with rounding

--- commands/stats/weight.js
+++ commands/stats/weight.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const config = require('../../config.json')
 const fetch = require('node-fetch');
 
 const loading = `819138970771652609`
@@ -57,38 +58,38 @@ module.exports = {
             new Discord.MessageEmbed()
                 .setAuthor(ign, `https://cravatar.eu/helmavatar/${ign}/600.png`, `https://sky.shiiyu.moe/stats/${ign}`)
                 .setColor('69e0a5')
-                .setDescription(`${ign}'s weights for their **${apiData.data.name}** profile are **${roundNumber(apiData.data.weight)} + ${roundNumber(apiData.data.weight_overflow)} Overflow (${roundNumber(apiData.data.weight) + roundNumber(apiData.data.weight_overflow)} Total)**`)
+                .setDescription(`${ign}'s weights for their **${apiData.data.name}** profile are **${(apiData.data.weight).toString().substr(0, 7)} + ${(apiData.data.weight_overflow).toString().substr(0, 5)} Overflow (${(apiData.data.weight + apiData.data.weight_overflow).toString().substr(0, 7)} Total)**`)
                 .addFields(
                     {
-                        name: `Skills Weight: ${roundNumber(apiData.data.skills.weight)} + ${roundNumber(apiData.data.skills.weight_overflow)} Overflow (${roundNumber(apiData.data.skills.weight) + roundNumber(apiData.data.skills.weight_overflow)} Total)`,
+                        name: `Skills Weight: ${(apiData.data.skills.weight).toString().substr(0, 6)} + ${(apiData.data.skills.weight_overflow).toString().substr(0, 5)} Overflow (${(apiData.data.skills.weight + apiData.data.skills.weight_overflow).toString().substr(0, 6)} Total)`,
                         value: '```ruby\n' + [
-                            `Mining     > Lvl: ${roundNumber(apiData.data.skills.mining.level)}     Weight: ${roundNumber(apiData.data.skills.mining.weight)}`,
-                            `Foraging   > Lvl: ${roundNumber(apiData.data.skills.foraging.level)}     Weight: ${roundNumber(apiData.data.skills.foraging.weight)}`,
-                            `Enchanting > Lvl: ${roundNumber(apiData.data.skills.enchanting.level)} Weight: ${roundNumber(apiData.data.skills.enchanting.weight)}`,
-                            `Farming    > Lvl: ${roundNumber(apiData.data.skills.farming.level)}    Weight: ${roundNumber(apiData.data.skills.farming.weight)}`,
-                            `Combat     > Lvl: ${roundNumber(apiData.data.skills.combat.level)}     Weight: ${roundNumber(apiData.data.skills.combat.weight)}`,
-                            `Fishing    > Lvl: ${roundNumber(apiData.data.skills.fishing.level)}    Weight: ${roundNumber(apiData.data.skills.fishing.weight)}`,
-                            `Alchemy    > Lvl: ${roundNumber(apiData.data.skills.alchemy.level)}    Weight: ${roundNumber(apiData.data.skills.alchemy.weight)}`,
-                            `Taming     > Lvl: ${roundNumber(apiData.data.skills.taming.level)}     Weight: ${roundNumber(apiData.data.skills.taming.weight)}`
+                            `Mining     > Lvl: ${(apiData.data.skills.mining.level).toString().substr(0, 5)}     Weight: ${(apiData.data.skills.mining.weight).toString().substr(0, 5)}`,
+                            `Foraging   > Lvl: ${(apiData.data.skills.foraging.level).toString().substr(0, 5)}     Weight: ${(apiData.data.skills.foraging.weight).toString().substr(0, 5)}`,
+                            `Enchanting > Lvl: ${(apiData.data.skills.enchanting.level).toString().substr(0, 5)} Weight: ${(apiData.data.skills.enchanting.weight).toString().substr(0, 5)}`,
+                            `Farming    > Lvl: ${(apiData.data.skills.farming.level).toString().substr(0, 5)}    Weight: ${(apiData.data.skills.farming.weight).toString().substr(0, 5)}`,
+                            `Combat     > Lvl: ${(apiData.data.skills.combat.level).toString().substr(0, 5)}     Weight: ${(apiData.data.skills.combat.weight).toString().substr(0, 5)}`,
+                            `Fishing    > Lvl: ${(apiData.data.skills.fishing.level).toString().substr(0, 5)}    Weight: ${(apiData.data.skills.fishing.weight).toString().substr(0, 5)}`,
+                            `Alchemy    > Lvl: ${(apiData.data.skills.alchemy.level).toString().substr(0, 5)}    Weight: ${(apiData.data.skills.alchemy.weight).toString().substr(0, 5)}`,
+                            `Taming     > Lvl: ${(apiData.data.skills.taming.level).toString().substr(0, 5)}     Weight: ${(apiData.data.skills.taming.weight).toString().substr(0, 5)}`
                         ].join('\n') + '\n```'
                     },
                     {
-                        name: `Slayer Weight: ${roundNumber(apiData.data.slayers.weight)} + ${roundNumber(apiData.data.slayers.weight_overflow)} Overflow (${roundNumber(apiData.data.slayers.weight) + roundNumber(apiData.data.slayers.weight_overflow)} Total)`,
+                        name: `Slayer Weight: ${(apiData.data.slayers.weight).toString().substr(0, 6)} + ${(apiData.data.slayers.weight_overflow).toString().substr(0, 5)} Overflow (${(apiData.data.slayers.weight + apiData.data.slayers.weight_overflow).toString().substr(0, 6)} Total)`,
                         value: '```ruby\n' + [
-                            `Revenant   > Exp: ${roundNumber(apiData.data.slayers.bosses.revenant.experience)}      Weight: ${roundNumber(apiData.data.slayers.bosses.revenant.weight)}`,
-                            `Tarantula  > Exp: ${roundNumber(apiData.data.slayers.bosses.tarantula.experience)}     Weight: ${roundNumber(apiData.data.slayers.bosses.tarantula.weight)}`,
-                            `Sven       > Exp: ${roundNumber(apiData.data.slayers.bosses.sven.experience)}          Weight: ${roundNumber(apiData.data.slayers.bosses.sven.weight)}`
+                            `Revenant   > Exp: ${(apiData.data.slayers.bosses.revenant.experience).toString().substr(0, 5)}      Weight: ${(apiData.data.slayers.bosses.revenant.weight).toString().substr(0, 5)}`,
+                            `Tarantula  > Exp: ${(apiData.data.slayers.bosses.tarantula.experience).toString().substr(0, 5)}     Weight: ${(apiData.data.slayers.bosses.tarantula.weight).toString().substr(0, 5)}`,
+                            `Sven       > Exp: ${(apiData.data.slayers.bosses.sven.experience).toString().substr(0, 5)}          Weight: ${(apiData.data.slayers.bosses.sven.weight).toString().substr(0, 5)}`
                         ].join('\n') + '\n```'
                     },
                     {
-                        name: `Dungeon Weight: ${roundNumber(apiData.data.dungeons.weight)} + ${roundNumber(apiData.data.dungeons.weight_overflow)} Overflow (${roundNumber(apiData.data.dungeons.weight) + roundNumber(apiData.data.dungeons.weight_overflow)} Total)`,
+                        name: `Dungeon Weight: ${(apiData.data.dungeons.weight).toString().substr(0, 6)} + ${(apiData.data.dungeons.weight_overflow).toString().substr(0, 5)} Overflow (${(apiData.data.dungeons.weight + apiData.data.dungeons.weight_overflow).toString().substr(0, 6)} Total)`,
                         value: '```ruby\n' + [
-                            `Catabombs  > Lvl: ${roundNumber(apiData.data.dungeons.types.catacombs.level)}          Weight: ${roundNumber(apiData.data.dungeons.types.catacombs.weight)}`,
-                            `Healer     > Lvl: ${roundNumber(apiData.data.dungeons.classes.healer.level)}           Weight: ${roundNumber(apiData.data.dungeons.classes.healer.weight)}`,
-                            `Mage       > Lvl: ${roundNumber(apiData.data.dungeons.classes.mage.level)}             Weight: ${roundNumber(apiData.data.dungeons.classes.mage.weight)}`,
-                            `Berserker  > Lvl: ${roundNumber(apiData.data.dungeons.classes.berserker.level)}        Weight: ${roundNumber(apiData.data.dungeons.classes.berserker.weight)}`,
-                            `Archer     > Lvl: ${roundNumber(apiData.data.dungeons.classes.archer.level)}           Weight: ${roundNumber(apiData.data.dungeons.classes.archer.weight)}`,
-                            `Tank       > Lvl: ${roundNumber(apiData.data.dungeons.classes.tank.level)}             Weight: ${roundNumber(apiData.data.dungeons.classes.tank.weight)}`
+                            `Catabombs  > Lvl: ${(apiData.data.dungeons.types.catacombs.level).toString().substr(0, 5)}          Weight: ${(apiData.data.dungeons.types.catacombs.weight).toString().substr(0, 5)}`,
+                            `Healer     > Lvl: ${(apiData.data.dungeons.classes.healer.level).toString().substr(0, 5)}           Weight: ${(apiData.data.dungeons.classes.healer.weight).toString().substr(0, 5)}`,
+                            `Mage       > Lvl: ${(apiData.data.dungeons.classes.mage.level).toString().substr(0, 5)}             Weight: ${(apiData.data.dungeons.classes.mage.weight).toString().substr(0, 5)}`,
+                            `Berserker  > Lvl: ${(apiData.data.dungeons.classes.berserker.level).toString().substr(0, 5)}        Weight: ${(apiData.data.dungeons.classes.berserker.weight).toString().substr(0, 5)}`,
+                            `Archer     > Lvl: ${(apiData.data.dungeons.classes.archer.level).toString().substr(0, 5)}           Weight: ${(apiData.data.dungeons.classes.archer.weight).toString().substr(0, 5)}`,
+                            `Tank       > Lvl: ${(apiData.data.dungeons.classes.tank.level).toString().substr(0, 5)}             Weight: ${(apiData.data.dungeons.classes.tank.weight).toString().substr(0, 5)}`
                         ].join('\n') + '\n```'
                     }
                 )
@@ -96,3 +97,22 @@ module.exports = {
         ).then(message.reactions.removeAll().catch(error => console.error('Failed to clear reactions: ', error)))
     },
 };
+
+async function getUUID(ign) {
+	const response = await fetch(`https://api.mojang.com/users/profiles/minecraft/${ign}`);
+    const result = await response.json();
+    const uuid = result.id;
+    return uuid.substr(0,8)+"-"+uuid.substr(8,4)+"-"+uuid.substr(12,4)+"-"+uuid.substr(16,4)+"-"+uuid.substr(20);
+}
+
+async function getApiData(ign) {
+	const UUID = await getUUID(ign);
+	const response = await fetch(`https://hypixel-api.senither.com/v1/profiles/${UUID}/save?key=${config.discord.apiKey}`);
+	return await response.json();
+}
+
+async function getTrueIgn(ign) {
+	const response = await fetch(`https://api.mojang.com/users/profiles/minecraft/${ign}`);
+    const result = await response.json();
+    return result.name;
+}
