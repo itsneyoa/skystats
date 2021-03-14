4d6fdc00b22f4e296f1fdb2502ff951f550eaffd
neyoa <itstomsmart@gmail.com>
2021-03-14 17:16:14 +0000

    Test if skills api is enabled

--- commands/stats/check.js
+++ commands/stats/check.js
@@ -1,12 +1,13 @@
 const Discord = require('discord.js');
 const config = require('../../config.json');
 const fetch = require('node-fetch');
+const chalk = require('chalk');
 
 module.exports = {
 	name: 'check',
     aliases: ['c', 'stats'],
 	description: 'Gets metrics about a player',
-	execute(message, args) {
+	async execute(message, args) {
         if(message.channel.type === 'dm' && !args[0]){
 			return message.channel.send(
 				new Discord.MessageEmbed()
@@ -19,8 +20,6 @@ module.exports = {
 			var ign = args[0];
 		} // Gets IGN
 
-		message.channel.send(`IGN: ${ign}`);
-
 		fetch(`https://api.mojang.com/users/profiles/minecraft/${ign}`)
     	.then(res => {
         	if(res.status != 200){
@@ -32,20 +31,29 @@ module.exports = {
 			}
     	}); // Test if IGN esists
 
-		// At this point we know its a valid IGN, but not if it has skyblock profiles
-		var apiData = await getApiData(ign); // Gets all skyblock player data from Senither's Hypixel API Facade
+		ign = await getTrueIgn(ign);
 
-		if(apiData.status == 404) return message.channel.send(
+		// At this point we know its a valid IGN, but not if it has skyblock profiles
+		const apiData = await getApiData(ign); // Gets all skyblock player data from Senither's Hypixel API Facade
+
+		if (apiData.status == 404) return message.channel.send(
 			new Discord.MessageEmbed()
 			.setDescription(`No Skyblock profile found for \`${ign}\``)
 			.setColor('DC143C')
 		)
 
 		// IGN is valid and player has skyblock profiles
+
+		if (apiData.data.skills.apiEnabled == false) return message.channel.send(
+			new Discord.MessageEmbed()
+			.setAuthor(ign, `https://cravatar.eu/helmavatar/${ign}/600.png`, `https://sky.shiiyu.moe/stats/${ign}`)
+			.setDescription('You currently have skills API disabled, please enable it in the skyblock menu and try again')
+			.setColor('DC143C')
+		)
 	},
 };
 
-async function getUUID(ign){
+async function getUUID(ign) {
 	const response = await fetch(`https://api.mojang.com/users/profiles/minecraft/${ign}`);
     const result = await response.json();
     const uuid = result.id;
@@ -56,4 +64,10 @@ async function getApiData(ign) {
 	const UUID = await getUUID(ign);
 	const response = await fetch(`https://hypixel-api.senither.com/v1/profiles/${UUID}/save?key=${config.discord.apiKey}`);
 	return await response.json();
+}
+
+async function getTrueIgn(ign) {
+	const response = await fetch(`https://api.mojang.com/users/profiles/minecraft/${ign}`);
+    const result = await response.json();
+    return result.name;
 }
