1627b86465b9f2a9dea22ae99fc08c94f0f2a936
itsneyoa <itstomsmart@icloud.com>
2021-03-14 14:21:28 +0000

    Reformatted help

--- commands/help.js
+++ commands/help.js
@@ -4,32 +4,33 @@ const package = require('../package.json');
 
 module.exports = {
 	name: 'help',
-    aliases: ['h', 'aaa'],
+    aliases: ['h', 'info'],
 	description: 'Gets information about the bot',
 	execute(message, args) {
-		message.channel.send(new Discord.MessageEmbed()
-        .setTitle('Help!')
-        .addFields(
-            {
-                name: "Commands",
-                value: [
-                    `Check player: \`${config.discord.prefix}check [ign] [profile]\``,
-                    `Help: \`${config.discord.prefix}help\``
-                ].join('\n'),
-                inline: true
-            },
-            {
-                name: "Info",
-                value: [
-                    `Prefix: \`${config.discord.prefix}\``,
-                    `Version: \`${package.version}\``
-                ].join('\n'),
-                inline: true
-            }
-        )
-        .setColor(message.guild.me.displayHexColor)
-        .setFooter('Made by neyoa ❤')
-        .setTimestamp()
+		message.channel.send(
+            new Discord.MessageEmbed()
+            .setTitle('Help!')
+            .addFields(
+                {
+                    name: "Commands",
+                    value: [
+                        `Check player: \`${config.discord.prefix}check [ign] [profile]\``,
+                        `Help: \`${config.discord.prefix}help\``
+                    ].join('\n'),
+                    inline: true
+                },
+                {
+                    name: "Info",
+                    value: [
+                        `Prefix: \`${config.discord.prefix}\``,
+                        `Version: \`${package.version}\``
+                    ].join('\n'),
+                    inline: true
+                }
+            )
+            .setColor(message.guild.me.displayHexColor)
+            .setFooter('Made by neyoa ❤')
+            .setTimestamp()
         )
 	},
 };
