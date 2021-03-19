ce3b78560c18885d44f4535ccc2b45bfe042fbe0
itsneyoa <itstomsmart@icloud.com>
2021-03-19 09:48:39 +0000

    Added hi command :)

---
+++ commands/other/hi.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'hi',
+    aliases: ['hello', 'o/'],
+    description: 'Says hi!',
+    execute(message, args) {
+        return message.channel.send(
+            new Discord.MessageEmbed()
+            .setTitle('o/')
+            .setDescription(`Hi ${message.author} :heart:`)
+            .setColor(Math.floor(Math.random()*16777215).toString(16))
+        )
+    },
+};
