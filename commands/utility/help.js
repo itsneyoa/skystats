916f8ea812ceca678f65cb76ff6373db48480551
itsneyoa <itstomsmart@icloud.com>
2021-03-19 23:32:59 +0000

    Started dynamic help command

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const fs = require('fs')
 
 const yes = `819295941621841970`;
 const no = `819295822230716467`;
@@ -15,60 +16,51 @@ module.exports = {
         delete require.cache[require.resolve('../../config.json')];
         const config = require('../../config.json');
 
-        message.author.send(
-            new Discord.MessageEmbed()
-                .setTitle('SkyStats Help')
-                .addFields(
-                    {
-                        name: "Stats",
-                        value: [
-                            'Maniacs check: `check [ign]` (`c`)',
-                            'Guild check :  `guildcheck [ign]` (`gc`, `gcheck`)',
-                            'Player weight:  `weight [ign]` (`we`)',
-                            'Player stats: `player [ign]` (`p`, `stats`)'
-                        ].join('\n'),
-                    },
-                    {
-                        name: "Utility",
-                        value: [
-                            'Help: `help` (`h`, `info`)',
-                            'Latency: `ping` (`lag`, `latency`)',
-                            'Reload: `reload <command>` (`r`, `reboot`)',
-                            'Status: `status <type> <message>` (`presence`, `setstatus`)'
-                        ].join('\n'),
-                    },
-                    {
-                        name: "Fun",
-                        value: [
-                            'Hi: `hi` (`hello`)',
-                            'Skin: `skin [ign]` (`sk`, `skull`, `head`)'
-                        ].join('\n'),
-                    },
-                    {
-                        name: "Info",
-                        value: [
-                            `Prefix: \`${config.discord.prefix}\``,
-                            `Version: \`${package.version}\``,
-                            `Issues: [click here](https://github.com/itsneyoa/skystats/issues)`,
-                            `Server: \`${message.guild.name}\``,
-                            `Channel: ${message.channel}`
-                        ].join('\n')
-                    }
-                )
-                .setColor(message.guild.me.displayHexColor)
-                .setFooter('Made by neyoa ❤')
-                .setTimestamp()
-        ).then(() => {
-            message.react(yes);
-        })
-            .catch(() => {
-                message.channel.send(
-                    new Discord.MessageEmbed()
-                        .setDescription(`${message.author}, I can't DM you! Make sure you have DMs enabled!`)
-                        .setColor('DC143C')
-                ).then(() => {
-                    message.react(no);
+        const commandFolders = fs.readdirSync('./commands');
+
+        if (!args.length) {
+            let embed = new Discord.MessageEmbed();
+            embed.setTitle(`Help - ${message.client.user.username}`)
+            
+            for (const folder of commandFolders) {
+                let data = [];
+                const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+                for (const file of commandFiles) {
+                    const command = require(`../${folder}/${file}`);
+                    let currentCommand = [];
+                    currentCommand.push(command.name);
+                    currentCommand.push('-');
+                    currentCommand.push(command.description);
+                    data.push(currentCommand.join(' '));
+                }
+                embed.addField((folder.charAt(0).toUpperCase() + folder.slice(1)), data.join('\n'))
+            }
+
+            return message.author.send(embed)
+                .then(() => {
+                    if (message.channel.type === 'dm') return;
+                    message.reply('I\'ve sent you a DM with all my commands!');
                 })
-            });
+                .catch(error => {
+                    console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
+                    message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
+                });
+        }
+
+        const name = args[0].toLowerCase();
+        const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
+
+        if (!command) {
+            return message.reply('that\'s not a valid command!');
+        }
+
+        data.push(`**Name:** ${command.name}`);
+
+        if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
+        if (command.description) data.push(`**Description:** ${command.description}`);
+
+        message.channel.send(data, { split: true });
+
+
     },
 };
