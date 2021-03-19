83fb305c79403eb41488b50b91b647ce8013e54d
neyoa <itstomsmart@gmail.com>
2021-03-19 17:39:52 +0000

    Added command to get player head image

---
+++ commands/fun/getskin.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'skin',
+    aliases: ['head', 'skull', 'sk'],
+    description: 'Gets player head from ign',
+    execute(message, args) {
+        if (!args[0]) {
+            var ign = message.member.displayName;
+        } else {
+            if (message.mentions.members.first()) {
+                var ign = message.mentions.members.first().displayName;
+            }
+            else var ign = args[0];
+        } // Gets IGN
+
+        ign = ign.replace(/\W/g, ''); // removes weird characters
+
+        return message.channel.send(
+            new Discord.MessageEmbed()
+                .setThumbnail('https://cravatar.eu/helmavatar/neyoa/600.png')
+                .setColor('7CFC00')
+        )
+    },
+};
