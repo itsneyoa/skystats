f45a6cecebe927499c8cc3a385198c8d03be116b
neyoa <itstomsmart@gmail.com>
2021-03-19 17:14:47 +0000

    Removed o/ hi alias

--- commands/other/hi.js
+++ commands/other/hi.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 module.exports = {
     name: 'hi',
-    aliases: ['hello', 'o/'],
+    aliases: ['hello'],
     description: 'Says hi!',
     execute(message, args) {
         return message.channel.send(
