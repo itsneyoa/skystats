17e5f36d93410db48dabcdd43c3bc59c1a288d32
neyoa <itstomsmart@gmail.com>
2021-03-23 15:14:14 +0000

    Reworked reload command

--- commands/utility/reload.js
+++ commands/utility/reload.js
@@ -8,46 +8,92 @@ module.exports = {
     description: 'Reloads a command',
     ownerOnly: true,
     execute(message, args) {
-        if (!args.length) return message.channel.send(
-            new Discord.MessageEmbed()
-                .setDescription(`You need to specify a command to reload`)
-                .setColor('DC143C')
-        );
-        const commandName = args[0].toLowerCase();
-        const command = message.client.commands.get(commandName)
-            || message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+        var commandsList = [];
 
-        if (!command) return message.channel.send(
-            new Discord.MessageEmbed()
-                .setDescription(`There is no command with name or alias \`${commandName}\``)
-                .setColor('DC143C')
-        );
+        var reloadSuccess = [];
+        var reloadFaliure = [];
 
-        const commandFolders = fs.readdirSync('./commands');
-        var folderName;
-        commandFolders.forEach(folder => {
-            const commandSubFolders = fs.readdirSync(`./commands/${folder}`);
-            commandSubFolders.forEach(cmd => {
-                if (cmd == `${command.name}.js`) folderName = folder;
-            });
+        if (!args.length) { // Reload all commands
+            const commandFolders = fs.readdirSync('./commands');
+
+            for (const folder of commandFolders) {
+                const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+                for (const file of commandFiles) {
+                    const command = require(`../${folder}/${file}`);
+                    commandsList.push(command.name)
+                }
+            }
+        }
+
+        else args.forEach(arg => {
+            commandsList.push(arg);
         });
 
-        delete require.cache[require.resolve(`../${folderName}/${command.name}.js`)];
+        commandsList.forEach(element => {
+            const commandName = element.toLowerCase();
+            const command = message.client.commands.get(commandName)
+                || message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
-        try {
-            const newCommand = require(`../${folderName}/${command.name}.js`);
-            message.client.commands.set(newCommand.name, newCommand);
-            return message.channel.send(
+            if (!command) return message.channel.send(
                 new Discord.MessageEmbed()
-                    .setDescription(`Command \`${command.name}\` successfully reloaded!`)
-                    .setColor('7CFC00')
-            )
-        } catch (error) {
-            console.error(error);
-            return message.channel.send(
-                new Discord.MessageEmbed()
-                    .setDescription(`Command \`${command.name}\` could not be reloaded.`)
+                    .setDescription(`There is no command with name or alias \`${commandName}\``)
                     .setColor('DC143C')
+            );
+
+            const commandFolders = fs.readdirSync('./commands');
+            var folderName;
+            commandFolders.forEach(folder => {
+                const commandSubFolders = fs.readdirSync(`./commands/${folder}`);
+                commandSubFolders.forEach(cmd => {
+                    if (cmd == `${command.name}.js`) folderName = folder;
+                });
+            });
+
+            delete require.cache[require.resolve(`../${folderName}/${command.name}.js`)];
+
+            try {
+                const newCommand = require(`../${folderName}/${command.name}.js`);
+                message.client.commands.set(newCommand.name, newCommand);
+                reloadSuccess.push(`\`${command.name}\``)
+            } catch (error) {
+                console.error(error);
+                reloadFaliure.push(`\`${command.name}\``)
+            }
+        });
+
+        if (reloadFaliure.length == 0) {
+            return message.channel.send(
+                new Discord.MessageEmbed()
+                    .addField('Reloaded:', reloadSuccess.join('\n'), true)
+                    .setColor('7CFC00')
+                    .setTimestamp()
+            )
+        } else if (reloadSuccess.length == 0) {
+            return message.channel.send(
+                new Discord.MessageEmbed()
+                    .setAuthor('Reload')
+                    .addFields('Failed:', reloadFaliure.join('\n'), true)
+                    .setColor('DC143C')
+                    .setTimestamp()
+            )
+        } else {
+            return message.channel.send(
+                new Discord.MessageEmbed()
+                    .setAuthor('Reload')
+                    .addFields(
+                        {
+                            Name: 'Success:',
+                            value: reloadSuccess.join('\n'),
+                            inline: true
+                        },
+                        {
+                            Name: 'Failed:',
+                            value: reloadFaliure.join('\n'),
+                            inline: true
+                        }
+                    )
+                    .setColor('FF8C00')
+                    .setTimestamp()
             )
         }
     },
