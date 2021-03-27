da0705ff21977b7cf0676d39a3a9c27781de0a74
itsneyoa <itstomsmart@icloud.com>
2021-03-27 23:59:37 +0000

    Created placeholder commands for dungeons, skills and slayer

---
+++ commands/metrics/dungeons.js
@@ -0,0 +1,98 @@
+const Discord = require('discord.js');
+const fetch = require('node-fetch');
+
+const loading = `819138970771652609`
+
+module.exports = {
+    name: 'dungeons',
+    aliases: ['cata', 'catacombs', 'dungeon'],
+    usage: 'dungeons [ign] [profile]',
+    description: "Gets players dungeons information",
+    async execute(message, args) {
+        if (!args[0]) {
+            var ign = message.member.displayName;
+        } else {
+            if (message.mentions.members.first()) {
+                var ign = message.mentions.members.first().displayName;
+            }
+            else var ign = args[0];
+        } // Gets IGN
+
+        var method = 'save';
+        if (args[1]) method = args[1];
+
+        ign = ign.replace(/\W/g, ''); // removes weird characters
+
+        message.react(loading);
+
+        fetch(`https://api.mojang.com/users/profiles/minecraft/${ign}`)
+            .then(res => {
+                if (res.status != 200) {
+                    return message.channel.send(
+                        new Discord.MessageEmbed()
+                            .setDescription(`No Minecraft account found for \`${ign}\``)
+                            .setColor('DC143C')
+                            .setTimestamp()
+                    ).then(message.reactions.removeAll().catch(error => console.error('Failed to clear reactions: ', error)))
+                }
+            }); // Test if IGN esists
+
+        ign = await getTrueIgn(ign);
+
+        // At this point we know its a valid IGN, but not if it has skyblock profiles
+        const apiData = await getApiData(ign, method); // Gets all skyblock player data from Senither's Hypixel API Facade
+
+        if (apiData.status == 404) {
+            if (apiData.reason == 'Failed to find a profile using the given strategy') {
+                return message.channel.send(
+                    new Discord.MessageEmbed()
+                        .setDescription(`Profile \`${method}\` not found for \`${ign}\``)
+                        .setColor('DC143C')
+                        .setTimestamp()
+                ).then(message.reactions.removeAll().catch(error => console.error('Failed to clear reactions: ', error)))
+            } else {
+                return message.channel.send(
+                    new Discord.MessageEmbed()
+                        .setDescription(`No Skyblock profile found for \`${ign}\``)
+                        .setColor('DC143C')
+                        .setTimestamp()
+                ).then(message.reactions.removeAll().catch(error => console.error('Failed to clear reactions: ', error)))
+            }
+        }
+
+        // IGN is valid and player has skyblock profiles
+
+        return message.channel.send( // EDIT THIS BIT
+            new Discord.MessageEmbed()
+                .setDescription(`Hi, this command is still being worked on at the moment!`)
+                .setColor('FF8C00')
+        ).then(message.reactions.removeAll().catch(error => console.error('Failed to clear reactions: ', error)))
+    },
+};
+
+async function getUUID(ign) {
+    const response = await fetch(`https://api.mojang.com/users/profiles/minecraft/${ign}`);
+    const result = await response.json();
+    const uuid = result.id;
+    return uuid.substr(0, 8) + "-" + uuid.substr(8, 4) + "-" + uuid.substr(12, 4) + "-" + uuid.substr(16, 4) + "-" + uuid.substr(20);
+}
+
+async function getApiData(ign, method) {
+    delete require.cache[require.resolve('../../config.json')];
+    const config = require('../../config.json');
+
+    const UUID = await getUUID(ign);
+    const response = await fetch(`https://hypixel-api.senither.com/v1/profiles/${UUID}/${method}?key=${config.discord.apiKey}`);
+    return await response.json();
+}
+
+async function getTrueIgn(ign) {
+    const response = await fetch(`https://api.mojang.com/users/profiles/minecraft/${ign}`);
+    const result = await response.json();
+    return result.name;
+}
+
+function toFixed(num) {
+    var re = new RegExp('^-?\\d+(?:\.\\d{0,' + (2 || -1) + '})?');
+    return num.toString().match(re)[0];
+}
